9290195a1a373b9c0efd114ae7f2ef25c6a587d8
bobo <102934154+loyejaotdiqr47123@users.noreply.github.com>
2024-03-09 09:14:39 +0000

    Update Commit

--- bin/update.bat
+++ bin/update.bat
@@ -1,9 +1,10 @@
 @echo off
 if not exist "%~dp0RDPWInst.exe" goto :error
 "%~dp0RDPWInst" -w
-ping -n 3 localhost > nul
-cmd.exe /C start "" "C:\Program Files\RDP Wrapper\RDP_CnC.exe"
+echo.
+goto :anykey
 :error
 echo [-] Installer executable not found.
 echo Please extract all files from the downloaded package or check your anti-virus.
+:anykey
 pause
