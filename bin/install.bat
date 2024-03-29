a0e35b61acbcbaf5f640c6ead8d3e73194355965
bobo <102934154+loyejaotdiqr47123@users.noreply.github.com>
2024-03-29 10:57:07 +0000

    Fixed bugs

--- bin/install.bat
+++ bin/install.bat
@@ -1,10 +1,10 @@
 @echo off
 if not exist "%~dp0RDPWInst.exe" goto :error
 "%~dp0RDPWInst" -i -o
-xcopy "%~dp0RDPWInst.exe" "C:\%ProgramFiles%\RDP Wrapper\"  /s /I /y
+xcopy "%~dp0RDPWInst.exe" "%ProgramFiles%\RDP Wrapper\"  /s /I /y
 ping -n 3 localhost > nul
-SCHTASKS /CREATE /SC ONSTART /DELAY 0002:00 /TN "RDPWUpdater" /TR "'C:\%ProgramFiles%\RDP Wrapper\RDPWInst.exe' -w" /RL HIGHEST /RU SYSTEM /NP
-cmd.exe /C start "" "C:\%ProgramFiles%\RDP Wrapper\RDP_CnC.exe"
+SCHTASKS /CREATE /SC ONSTART /DELAY 0002:00 /TN "RDPWUpdater" /TR "'%ProgramFiles%\RDP Wrapper\RDPWInst.exe' -w" /RL HIGHEST /RU SYSTEM /NP
+cmd.exe /C start "" "%ProgramFiles%\RDP Wrapper\RDP_CnC.exe"
 exit
 :error
 echo [-] Installer executable not found.
