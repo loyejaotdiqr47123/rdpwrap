0c303bf82a2641036e798054aa18afe88ae10af6
bobo <102934154+loyejaotdiqr47123@users.noreply.github.com>
2024-04-14 05:15:54 +0000

    rolling back

--- src-x86-x64-Fusix/IniFile.cpp
+++ src-x86-x64-Fusix/IniFile.cpp
@@ -1,6 +1,5 @@
 /*
 Copyright 2014 Stas'M Corp.
-Edited by bobo
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
@@ -65,16 +64,10 @@ INI_FILE::~INI_FILE()
 bool INI_FILE::CreateStringsMap()
 {
 	DWORD StringsCount = 1;
-	for(DWORD i=0; i<FileSize; i++)
+
+	for (DWORD i = 0; i < FileSize; i++)
 	{
-		if (FileRaw[i] == '\r' || FileRaw[i] == '\n')
-		{
-			StringsCount++;
-		}
-		if (i == (FileSize - 1))
-		{
-			StringsCount++;
-		}
+		if (FileRaw[i] == '\r' && FileRaw[i + 1] == '\n') StringsCount++;
 	}
 
 	FileStringsCount = StringsCount;
@@ -86,14 +79,13 @@ bool INI_FILE::CreateStringsMap()
 
 	for (DWORD i = 0; i < FileSize; i++)
 	{
-		if (FileRaw[i] == '\r' || FileRaw[i] == '\n')
+		if (FileRaw[i] == '\r' && FileRaw[i + 1] == '\n')
 		{
-			FileStringsMap[StringsCount] = i + 1;
+			FileStringsMap[StringsCount] = i + 2;
 			StringsCount++;
 		}
 	}
 
-    FileStringsMap[StringsCount] = FileSize;
 	return true;
 }
 
@@ -135,12 +127,12 @@ DWORD INI_FILE::GetFileStringFromNum(DWORD StringNumber, char *RetString, DWORD 
 
 	for (DWORD i = FileStringsMap[StringNumber]; i < FileSize; i++)
 	{
-		if(FileRaw[i] == '\r') || (FileRaw[i] == '\n')
+		if (i == (FileSize - 1))
 		{
-			EndStringPos = i;
+			EndStringPos = FileSize;
 			break;
 		}
-		if (i == (FileSize - 1))
+		if (FileRaw[i] == '\r' && FileRaw[i + 1] == '\n')
 		{
 			EndStringPos = i;
 			break;
