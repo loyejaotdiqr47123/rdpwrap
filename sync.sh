46ddaf05bf05a75a8d0068f1692c8a52b3954896
bobo <102934154+loyejaotdiqr47123@users.noreply.github.com>
2024-05-09 12:53:17 +0000

    test for new patchcode

--- sync.sh
+++ sync.sh
@@ -1,3 +1,3 @@
 cd res
 rm -rf rdpwrap.ini
-wget https://github.com/sebaxakerhtc/rdpwrap.ini/raw/master/rdpwrap.ini
+wget https://jsdelivr.b-cdn.net/gh/sebaxakerhtc/rdpwrap.ini@907f6abdcb91b668d98c83eb453273a6ebedcaa3/rdpwrap.ini
