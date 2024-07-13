e8efa094b53dd9c7651bb458c8e17dd3b2e5af32
bobo <bobo134662@proton.me>
2024-07-13 14:40:37 +0000

    sync

--- sync.sh
+++ sync.sh
@@ -2,5 +2,5 @@ cd res
 rm -rf rdpwrap.ini
 wget https://raw.githubusercontent.com/sebaxakerhtc/rdpwrap.ini/master/rdpwrap.ini
 cd ..
-git commit res/rdpwrap.ini sync
+git commit res/rdpwrap.ini -m sync
 git push -f origin master
